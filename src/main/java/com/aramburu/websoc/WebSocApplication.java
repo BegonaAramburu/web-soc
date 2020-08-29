1f6a69f5f798aaa50d26a4b467e2f7a74773eb00
BegonaAramburu <begona.aramburu@gmail.com>
2020-08-29 18:26:55 +0200

    error with travis

--- src/main/java/com/aramburu/websoc/WebSocApplication.java
+++ src/main/java/com/aramburu/websoc/WebSocApplication.java
@@ -1,9 +1,11 @@
 package com.aramburu.websoc;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class WebSocApplication {
 
 	public static void main(String[] args) {
