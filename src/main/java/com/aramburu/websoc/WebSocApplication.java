d42e46a753d52d141dfa14cacd3adb2bad24696a
BegonaAramburu <begona.aramburu@gmail.com>
2020-08-29 16:16:54 +0200

    create local repository

---
+++ src/main/java/com/aramburu/websoc/WebSocApplication.java
@@ -0,0 +1,14 @@
+package com.aramburu.websoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/aramburu/websoc/WebSocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aramburu.websoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
